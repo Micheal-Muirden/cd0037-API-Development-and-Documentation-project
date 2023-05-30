647ef7b9157c13dc38e19845246e2f8b76cccd37
PH27HW@DIN.BCZ.GOV.AU <micheal.muirden@homeaffairs.gov.au>
2023-05-30 12:14:16 +1000

    Got first test running

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -16,23 +16,34 @@ class TriviaTestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.database_name = "trivia_test"
         self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
 
+        # All of these aren't needed, and in fact throw an error in more recent versions of SQL Alchemy 
+        
+        # setup_db(self.app, self.database_path)
         # binds the app to the current context
-        with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
-            # create all tables
-            self.db.create_all()
+        # with self.app.app_context():
+        #     self.db = SQLAlchemy()
+        #     self.db.init_app(self.app)
+        #     # create all tables
+        #     self.db.create_all()
     
     def tearDown(self):
         """Executed after reach test"""
         pass
 
     """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
+    TODO Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_get_all_questions(self):
+        res = self.client().get("/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["questions"]))
+        self.assertTrue(len(data["categories"]))
+        self.assertEqual(data["current_category"], "All")
 
 
 # Make the tests conveniently executable
